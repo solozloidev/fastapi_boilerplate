b6edec3cc27aecffad38080c8e594625a69b03f3
solodev <solozloidev@gmail.com>
2025-04-02 18:11:52 +0200

    add api config to config

--- fastapi-app/config/config.py
+++ fastapi-app/config/config.py
@@ -6,7 +6,11 @@ class RunConfig(BaseModel):
     host: str = "localhost"
     port: int = 8000
 
+class APIConfig(BaseModel):
+    prefix: str = "/api"
+
 class Config(BaseSettings):
     run: RunConfig = RunConfig()
+    api: APIConfig = APIConfig()
 
 config = Config()
