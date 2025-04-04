6fa037710587e66550903cc2b9998c5e23a25c34
solodev <solozloidev@gmail.com>
2025-04-04 17:34:10 +0200

    add lifespan contreller

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,11 +1,25 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from config.config import config
+from utils import db_helper
 
-app = FastAPI()
-app.include_router(api_router)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    print("dispose database engine")
+    await  db_helper.dispose()
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(api_router)
+
+
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host=config.run.host, port=config.run.port, reload=True)
+    uvicorn.run(app="main:main_app", host=config.run.host, port=config.run.port, reload=True)
