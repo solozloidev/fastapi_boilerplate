24fa903291843642237358500f8f04cce1831735
solodev <solozloidev@gmail.com>
2025-04-04 18:40:50 +0200

    update metadata with naming convection

--- fastapi-app/models/base.py
+++ fastapi-app/models/base.py
@@ -1,7 +1,12 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+from config.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     id: Mapped[int] = mapped_column(primary_key=True)
